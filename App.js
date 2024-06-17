22cc8d1109e1db7c2e44194063fcb1d2958bb4ad
gilbert sahyoun <gilbertsahyoun.business@gmail.com>
2024-06-17 14:14:37 +0300

    Initial Commit

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your Goal!" />
+        <Button title="Add" />
+      </View>
+      <View>
+        <Text>List of goals... </Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 16,
+    backgroundColor: "white",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textInput: {
+    width: "80%",
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    paddingRight: 8,
+    paddingLeft: 8,
   },
 });
